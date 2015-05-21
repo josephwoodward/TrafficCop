4394bf2350b93e5b1d4d86041527b70f7240159f
JoeMighty <josephwoodward@xeuse.com>
2015-05-21 14:42:32 +0100

    Started work on TrafficCopAttribute

---
+++ TrafficCop.Core/Attribute/TrafficCopAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TrafficCop.Core.Attribute
+{
+    public class TrafficCopAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            /*if (!HttpContext.Current.Request.Browser.Crawler)
+            {
+                filterContext.Result = new ViewResult() {ViewName = "NotFound"};
+            }*/
+        }
+    }
+}
